cc092c71bd541d21c1e2dab8c5aec2396c039ef1
Theo McCray <tlmccraycray@gmail.com>
2024-01-16 11:17:43 -0700

    Add basic button functionality)

--- src/scripts.js
+++ src/scripts.js
@@ -9,3 +9,22 @@ import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+// Targeting and Event Listeners
+document.getElementById('login-btn').addEventListener('click', function() {
+    var loginForm = document.getElementById('login-form');
+    loginForm.classList.toggle('hidden');
+});
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log('Logged in with credentials:', username, password)
+});
+
+document.getElementById('view-rooms').addEventListener('click', function() {
+    //logic to fetch the room data and display it
+    console.log('Fetching and displaying available rooms');
+    // rplace this later with actual functionality
+});
